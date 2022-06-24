978bdff751582e4daff323146c179d1a9a4a2ae8
Hot Mama <hotmama@Hots-MacBook-Pro.local>
2022-06-24 11:14:40 -0700

    InitialCommit

---
+++ DayTripGenerator.py
@@ -0,0 +1,34 @@
+
+# Day Trip Genertor
+
+from random import random
+
+
+print (' Hello and Welcome to Day Trip Tours!')
+
+
+destination_cities = ['Eugene, OR','Los Angeles, CA','Sacramento, CA']
+def destination_cities
+for destination_cities in destination_cities:
+    print 
+
+result= random.choice (destination_cities)
+print(result)
+
+
+
+restaurant_eugene= ['The Vintage', 'Lion and Owl', 'Cornucopia Restaurant']
+
+
+restaurant_los_angeles= ['Otium', '71Above','Orsa & Winston']
+
+
+restaurant_sacramento= ['Mikuni','Iron Hourse Tavern','Ella Dining Room & Bar']
+
+transportaton_mode= ['Car Rental','Uber/Lyft','Taxi']
+
+entertainment_eugene= ['Attend a University of Oregon Ducks football game at Autzen Stadium', 'Ride the Willamette River bike trail','Take a tour of The Better Living Room by Ninkasi Brewing']
+
+entertainment_los_angeles= ['Attend a Los Angeles Dodgers baseball game', 'Take a Hollywood and celebrity homes bus tour','Go to Universal Sudios theme park']
+
+entertainment_sacramento= ['Attend a Sacramento Kings basketball game','Sacramento Valley beer tasting train ride','Tour Old Sacramento and the State Capitol']
